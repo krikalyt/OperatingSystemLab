7ff947173799ef2b3ad8778ed1c9899ceb831355
krishna <krikalyt@gmail.com>
2019-03-14 15:21:03 +0530

    before day 5

---
+++ Day 5/openxterm4.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+#include <sys/wait.h>
+#include <unistd.h>
+int main(int num,char** args)
+{
+  char* arg = args[1];
+  pid_t pid;
+  char line[80];
+  int n;
+  int FDs[2];
+  pipe(FDs);
+  pid = fork();
+  if(pid==0)
+  {
+    close(FDs[1]);
+    dup2(FDs[0],0);
+    char* abc = new char[10];
+    cin >> abc;
+    execl("/usr/bin/gnome-terminal","/usr/bin/gnome-terminal","--","man",abc,NULL);
+    exit(0);
+  }
+  else{
+    close(FDs[0]);
+    dup2(FDs[1],1);
+    execl("/bin/echo","/bin/echo",arg,NULL);
+    wait(0);
+  }
+  return 0;
+}
